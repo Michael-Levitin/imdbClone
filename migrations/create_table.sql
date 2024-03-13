d21f6e2e50963c5f269e66ccf562966fd37a078b
Michael Levitin <stgriard@yandex.ru>
2024-03-13 23:49:02 +0300

    added migrations

---
+++ migrations/create_table.sql
@@ -0,0 +1,57 @@
+DROP TABLE IF EXISTS parts;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors;
+
+CREATE TABLE movies
+(
+    id          serial primary key,
+    movie       varchar(150) not null,
+    description varchar(1000),
+    release     timestamp,
+    rating      float,
+    removed     bool,
+    created_at  timestamp
+);
+
+comment on column movies.id is 'id записи';
+comment on column movies.movie is 'название фильма';
+comment on column movies.description is 'описание';
+comment on column movies.release is 'дата выпуска';
+comment on column movies.rating is 'рейтинг';
+comment on column movies.removed is 'был удален';
+comment on column movies.created_at is 'дата создания записи';
+
+
+CREATE TABLE actors
+(
+    id         serial primary key,
+    name       varchar(60) not null,
+    dob        timestamp,
+    removed    bool,
+    created_at timestamp
+);
+
+comment on column actors.id is 'id записи';
+comment on column actors.name is 'полное имя';
+comment on column actors.dob is 'дата рождения';
+comment on column actors.removed is 'был удален';
+comment on column actors.created_at is 'дата создания записи';
+
+CREATE TABLE parts
+(
+    movie_id INTEGER NOT NULL,
+    actor_id INTEGER NOT NULL,
+    CONSTRAINT unique_parts UNIQUE (movie_id, actor_id)
+);
+
+comment on column parts.movie_id is 'id фильма';
+comment on column parts.actor_id is 'id актера';
+
+ALTER TABLE parts
+    ADD CONSTRAINT movie_id
+        foreign key (movie_id) references movies (id);
+
+ALTER TABLE parts
+    ADD CONSTRAINT actor_id
+        foreign key (actor_id) references actors (id);
+
---
+++ migrations/insert_data.sql
@@ -0,0 +1,29 @@
+INSERT INTO movies (movie, description, release, rating, removed, created_at)
+VALUES ('Braveheart',
+        'Braveheart is a 1995 American epic historical drama film directed by, produced by, and starring Mel Gibson. Gibson portrays Sir William Wallace, a late-13th century Scottish warrior who led the Scots in the First War of Scottish Independence against King Edward I of England. The film also stars Sophie Marceau, Patrick McGoohan and Catherine McCormack. The story is inspired by Blind Harry''s 15th century epic poem The Actes and Deidis of the Illustre and Vallyeant Campioun Schir William Wallace and was adapted for the screen by Randall Wallace.',
+        now(), 8.3, false, now()),
+       ('Edge of Tomorrow',
+        'Edge of Tomorrow[a] is a 2014 American science fiction action film directed by Doug Liman and written by Christopher McQuarrie and the writing team of Jez and John-Henry Butterworth, loosely based on the Japanese novel All You Need Is Kill by Hiroshi Sakurazaka. Starring Tom Cruise and Emily Blunt, the film takes place in a future where most of Europe is occupied by an alien race. Major William Cage (Cruise), a public relations officer with limited combat experience, is forced by his superiors to join a landing operation against the aliens, only to find himself experiencing a time loop as he tries to find a way to defeat the invaders. Bill Paxton and Brendan Gleeson also cast as supporting roles.',
+        now(), 7.9, false, now()),
+    ('DragonHeart', 'Dragonheart (stylized as DragonHeart) is a 1996 fantasy adventure film directed by Rob Cohen and written by Charles Edward Pogue, based on a story created by him and Patrick Read Johnson. The film stars Dennis Quaid, David Thewlis, Pete Postlethwaite, Dina Meyer, and Sean Connery as the voice of Draco. It was nominated for the Academy Award for Best Visual Effects and various other awards in 1996 and 1997. The film received mixed reviews, with critics praising the premise, visual effects, and character development but panning the script as confusing and clichéd. It was a box-office success, earning $115 million worldwide. It was dedicated to the memory of Steve Price and Irwin Cohen.', now(), 6.4, false, now());
+
+INSERT INTO actors (name, dob, removed, created_at)
+VALUES ('Mel Gibson', now(), false, now()),
+       ('Sophie Marceau', now(), false, now()),
+       ('Patrick McGoohan', now(), false, now()),
+       ('Catherine McCormack', now(), false, now()),
+       ('Tom Cruise', now(), false, now()),
+       ('Emily Blunt', now(), false, now()),
+       ('Tom Hanks', now(), false, now()),
+       ('Sharon Stone', now(), false, now());
+
+
+INSERT INTO parts (movie_id, actor_id)
+VALUES (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (2, 5),
+       (2, 6);
+
+
