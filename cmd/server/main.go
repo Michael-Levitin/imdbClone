bf2da8d34a556cdbc0e8c1a34c86980136f464e1
Michael Levitin <stgriard@yandex.ru>
2024-03-13 20:36:21 +0300

    added server

---
+++ cmd/server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/Michael-Levitin/imdbClone/config"
+	"github.com/Michael-Levitin/imdbClone/internal/database"
+	"github.com/Michael-Levitin/imdbClone/internal/delivery"
+	"github.com/Michael-Levitin/imdbClone/internal/logic"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	// загружаем конфиг
+	config.Init()
+	sc := config.New()
+	//logger := zerolog.New(os.Stdout)
+	zerolog.SetGlobalLevel(sc.LogLevel)
+
+	// подключаемся к базе данных
+	dbAdrr := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", sc.DbUsername, sc.DbPassword, sc.DbHost, sc.DbPort, sc.DbName)
+	db, err := pgxpool.New(context.TODO(), dbAdrr)
+	if err != nil {
+		log.Fatal().Err(err).Msg("error connecting to database")
+	}
+	log.Info().Msg("connected to database")
+	defer db.Close()
+
+	cloneDB := database.NewImdbCloneDB(db)                 // подключаем бд
+	cloneLogic := logic.NewImdbCloneLogic(cloneDB)         // подключаем бд к логике...
+	cloneServer := delivery.NewImdbCloneServer(cloneLogic) // ... а логику в delivery
+
+	http.HandleFunc("/findMoviesActors", cloneServer.FindMoviesActors)
+	http.HandleFunc("/removeMovies", cloneServer.RemoveMovies)
+	http.HandleFunc("/removeActors", cloneServer.RemoveActors)
+	log.Info().Msg("server is running...")
+	err = http.ListenAndServe(":8080", nil)
+	log.Fatal().Err(err).Msg("http server crashed")
+}
